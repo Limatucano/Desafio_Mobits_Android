0b9af2ee78b77b45578fdd38abece580f7ae1255
matheus lima <matheusmt31@gmail.com>
2022-02-09 15:03:47 -0300

    adicionado appBar e açao de voltar

---
+++ app/src/main/java/com/example/desafio_mobits_android/view/LivrosActivity.java
@@ -0,0 +1,20 @@
+package com.example.desafio_mobits_android.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.desafio_mobits_android.databinding.ActivityLivrosBinding;
+
+public class LivrosActivity extends AppCompatActivity {
+    private ActivityLivrosBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLivrosBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonBack.setOnClickListener(view -> {
+            onBackPressed();
+        });
+    }
+
+}
--- app/src/main/java/com/example/desafio_mobits_android/view/MainActivity.java
+++ app/src/main/java/com/example/desafio_mobits_android/view/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.desafio_mobits_android.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.desafio_mobits_android.R;
+import com.example.desafio_mobits_android.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonBooks.setOnClickListener(view -> {
+            Intent intent = new Intent(this, LivrosActivity.class);
+            startActivity(intent);
+        });
+
+        binding.buttonHouses.setOnClickListener(view -> {
+            Intent intent = new Intent(this, CasasActivity.class);
+            startActivity(intent);
+        });
     }
 }
